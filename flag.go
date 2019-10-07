4115e2120dac9c6ed0fa443629776f6fc08b428b
Timo Savola <timo.savola@iki.fi>
2019-10-07 20:26:53 +0300

    overridable default config files

--- flag.go
+++ flag.go
@@ -5,7 +5,9 @@
 package confi
 
 import (
+	"errors"
 	"flag"
+	"log"
 )
 
 // FileReader makes a ``dynamic value'' which reads files into the
@@ -41,5 +43,108 @@ func (a assigner) Set(expr string) error {
 }
 
 func (assigner) String() string {
+	return ""
+}
+
+// Buffer configuration files and assignments.
+type Buffer struct {
+	list []buffered
+}
+
+func NewBuffer(optionalDefaultFilename string) *Buffer {
+	log.Printf("NewBuffer(%q)", optionalDefaultFilename)
+	b := new(Buffer)
+	if optionalDefaultFilename != "" {
+		b.list = append(b.list, buffered{
+			filename: optionalDefaultFilename,
+			optional: true,
+		})
+	}
+	return b
+}
+
+// FileReplacer makes a ``dynamic value'' which buffers file names to be read.
+// It discards previously buffered values.
+func (b *Buffer) FileReplacer() flag.Value {
+	return bufferedFileReader{b, true}
+}
+
+// FileReader makes a ``dynamic value'' which buffers file names to be read.
+func (b *Buffer) FileReader() flag.Value {
+	return bufferedFileReader{b, false}
+}
+
+// Assigner makes a ``dynamic value'' which buffers assignment expressions to
+// be applied.
+func (b *Buffer) Assigner() flag.Value {
+	return bufferedAssigner{b}
+}
+
+// Apply files and assignments to the configuration.
+func (b Buffer) Apply(config interface{}) error {
+	log.Print(b)
+	for _, entry := range b.list {
+		if err := entry.apply(config); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+type buffered struct {
+	filename string
+	expr     string
+	optional bool
+}
+
+func (b buffered) apply(config interface{}) error {
+	switch {
+	case b.filename != "":
+		if b.optional {
+			return ReadFileIfExists(b.filename, config)
+		} else {
+			return ReadFile(b.filename, config)
+		}
+
+	case b.expr != "":
+		return Assign(config, b.expr)
+	}
+
+	panic(b)
+}
+
+type bufferedFileReader struct {
+	b       *Buffer
+	replace bool
+}
+
+func (fr bufferedFileReader) Set(filename string) error {
+	if filename == "" {
+		return errors.New("configuration file name is empty")
+	}
+	if fr.replace {
+		fr.b.list = nil
+	}
+	fr.b.list = append(fr.b.list, buffered{filename: filename})
+	return nil
+}
+
+func (bufferedFileReader) String() string {
+	return ""
+}
+
+type bufferedAssigner struct {
+	b *Buffer
+}
+
+func (a bufferedAssigner) Set(expr string) error {
+	if expr == "" {
+		return errors.New("configuration expression is empty")
+	}
+	a.b.list = append(a.b.list, buffered{expr: expr})
+	return nil
+}
+
+func (bufferedAssigner) String() string {
 	return ""
 }
